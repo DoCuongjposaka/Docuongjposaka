25728f43a56dec5c9504b48ffa89312a4af264a1
Docuong <docuong1997bn@gmail.com>
2021-07-18 18:40:56 +0900

    kiem tra file css

---
+++ app/card.js
@@ -0,0 +1,20 @@
+// DOM : 
+console.log("Hello");
+//tim cac elements 
+// document.getElementById("")
+// document.getElementsByTagName("") => HTMLCollection
+// document.getElementsByClassName("") => HTMLCollection
+let card = document.querySelector(".card")
+console.log(card);
+card.addEventListener("click",() =>{
+    card.classList.toggle("is-flipped")
+    //toggle : la mot cong tac. tim thay thi tat. ko tim thay thi bat
+    
+})
+// them elements 
+
+// xoa cac elements
+
+//style 
+
+// edit attributes
